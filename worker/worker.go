cac9ddaccac1b4682060be61b0de1920f43f7faa
Sven Kreiss <me@svenkreiss.com>
2016-03-09 09:16:42 -0500

    move to supervisor. First healthy workers.

--- worker/worker.go
+++ worker/worker.go
@@ -1,8 +1,15 @@
 package main
 
-
 import "fmt"
+import "log"
+import "net/http"
 
 func main() {
     fmt.Println("hello worker")
+
+    http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "pong")
+    })
+
+    log.Fatal(http.ListenAndServe(":5060", nil))
 }
